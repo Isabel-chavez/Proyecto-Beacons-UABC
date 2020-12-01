13f18a178611cafc88bc66b1c35c2c529ce7d6b6
Isabel-chavez <73866172+Isabel-chavez@users.noreply.github.com>
2020-12-01 10:03:20 -0800

    validando datos de ususario
    Se creo tabla en la base de datos y se hizo validación de usuarios

--- validar_acceso.php
+++ validar_acceso.php
@@ -4,22 +4,22 @@
   $email =$_POST['user_email'];        echo "emaial:".$email;
   $password=$_POST['user_clave'];      echo "<br>Password:".$password;
 
-  // $mysqli = new mysqli("localhost","chavez", "phoenix", "beacons");
+  $mysqli = new mysqli("localhost","chavez", "phoenix", "beacons");
   // $mysqli = new mysqli("localhost","geoconst_ichavez", "Miclave2020","geoconst_beacons");
 
-  // $resultado = $mysqli->query("SELECT * FROM usuarios_beacons WHERE email='$email' and clave='$password'");
-  // $cantidad_registros = $resultado->num_rows;
+  $resultado = $mysqli->query("SELECT * FROM usuarios_beacons WHERE correo='$email' and clave='$password'");
+  $cantidad_registros = $resultado->num_rows;
 
- /*
+ 
      if($cantidad_registros !=1)
 	   echo "Son correctos los datos";
      else
        echo "Datos Incorrectos!!";
- */      
+   
 
 /*
    while($fila=$resultado->fetch_assoc()){ 
-   	$correo=$fila["emial"];
+   	$correo=$fila["correo"];
    	$passwd=$fila["clave"];
    }	
 */
