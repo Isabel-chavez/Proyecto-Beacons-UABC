0265877e1227fd130619dc7a540268ff34c9fc96
Isabel-chavez <73866172+Isabel-chavez@users.noreply.github.com>
2020-12-01 09:26:17 -0800

    preparando codigo para validar
    Se creo archivo para validar  acceso al portal

---
+++ validar_acceso.php
@@ -0,0 +1,27 @@
+<?php
+
+  echo "VALIDAREMOS EL LOGIN<br>";
+  $email =$_POST['user_email'];        echo "emaial:".$email;
+  $password=$_POST['user_clave'];      echo "<br>Password:".$password;
+
+  // $mysqli = new mysqli("localhost","chavez", "phoenix", "beacons");
+  // $mysqli = new mysqli("localhost","geoconst_ichavez", "Miclave2020","geoconst_beacons");
+
+  // $resultado = $mysqli->query("SELECT * FROM usuarios_beacons WHERE email='$email' and clave='$password'");
+  // $cantidad_registros = $resultado->num_rows;
+
+ /*
+     if($cantidad_registros !=1)
+	   echo "Son correctos los datos";
+     else
+       echo "Datos Incorrectos!!";
+ */      
+
+/*
+   while($fila=$resultado->fetch_assoc()){ 
+   	$correo=$fila["emial"];
+   	$passwd=$fila["clave"];
+   }	
+*/
+ 
+?>
