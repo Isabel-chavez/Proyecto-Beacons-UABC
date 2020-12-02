e6ebbfc8750e8cb94eb390c14e2130ad07ac9f7d
Isabel-chavez <73866172+Isabel-chavez@users.noreply.github.com>
2020-12-01 18:04:03 -0800

    se cambio conexion local a producción
    Se habilito la conexion del servidor de producción para subirlo al servidor

--- validar_acceso.php
+++ validar_acceso.php
@@ -1,11 +1,11 @@
 <?php
 
-  echo "VALIDAREMOS EL LOGIN<br>";
+  // echo "VALIDAREMOS EL LOGIN<br>";
   $email =$_POST["user_email"];        // echo "emaial:".$email;
   $password=$_POST["user_clave"];      // echo "<br>Password:".$password;
 
-  $mysqli = new mysqli("localhost","chavez", "phoenix", "beacons");
-  // $mysqli = new mysqli("localhost","geoconst_ichavez", "Miclave2020","geoconst_beacons");
+  // $mysqli = new mysqli("localhost","chavez", "phoenix", "beacons");
+  $mysqli = new mysqli("localhost","geoconst_ichavez", "Miclave2020","geoconst_beacons");
 
   $resultado = $mysqli->query("SELECT * FROM usuarios_beacons WHERE correo='$email' and clave='$password'");
   $cantidad_registros = $resultado->num_rows;
