6755707b07fda68a82945a7aa2f496c52b8acb6e
Isabel-chavez <73866172+Isabel-chavez@users.noreply.github.com>
2020-12-01 18:08:58 -0800

    conexion base de datos
    se comento la conexion a producción

--- validar_acceso.php
+++ validar_acceso.php
@@ -4,8 +4,8 @@
   $email =$_POST["user_email"];        // echo "emaial:".$email;
   $password=$_POST["user_clave"];      // echo "<br>Password:".$password;
 
-  // $mysqli = new mysqli("localhost","chavez", "phoenix", "beacons");
-  $mysqli = new mysqli("localhost","geoconst_ichavez", "Miclave2020","geoconst_beacons");
+  $mysqli = new mysqli("localhost","chavez", "phoenix", "beacons");
+  //$mysqli = new mysqli("localhost","geoconst_ichavez", "Miclave2020","geoconst_beacons");
 
   $resultado = $mysqli->query("SELECT * FROM usuarios_beacons WHERE correo='$email' and clave='$password'");
   $cantidad_registros = $resultado->num_rows;
